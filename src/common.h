5c3df0ad878328596df93cf38bad45d80e8ab85a
qart <thissak@gmail.com>
2024-03-24 09:58:42 +0900

    add commo 헤더파일

---
+++ src/common.h
@@ -0,0 +1,19 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <memory>
+#include <string>
+#include <optional>
+#include <spdlog/spdlog.h>
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+#define CLASS_PTR(KlassName) \
+class klassName; \
+using klassName ## UPtr = std::unique_ptr<klassName>; \
+using klassName ## Ptr = std::shared_ptr<klassName>; \
+using klassName ## WPtr = std::weak_ptr<klassName>;
+
+std::optional<std::string> LoadTextFile(const std::string& filename);
+
+#endif //__COMMON_H__
